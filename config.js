6f488232c506d69023963a364feccec809561c15
tompsen99 <89882283+tompsen99@users.noreply.github.com>
2026-04-10 14:20:20 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const SITE_CONFIG = {
+  SECRET_KEY: "ChangeThisToYourOwnSuperSecretKey_2026!",
+  STORAGE: {
+    LICENSE: "teacher_pro_license",
+    USAGE: "cloudTextUsage",
+    ADMIN_AUTH: "admin_auth_session"
+  },
+  SITE_NAME: "教师工具站",
+  VERSION: "1.0.0"
+};
